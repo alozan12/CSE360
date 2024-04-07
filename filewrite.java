14f966489010e372e8b7768fc7db5f82bd1fbbd6
aaravbodla <147215351+aaravbodla@users.noreply.github.com>
2024-04-07 01:43:42 -0700

    Update with controll class filewrite.java

--- filewrite.java
+++ filewrite.java
@@ -6,22 +6,22 @@ import java.io.File;
 
 public class filewrite {
 	
-//	private String firstname;
-//	private String lastname;
-//	private String data;
-	private String filename;
+	private String firstname;
+	private String lastname;
+	private String data;
+	private static String filename;
 	
 	public filewrite() {
 	}
 	
-	public void createprofile(String firstname,String lastname, String dob) {
+	public static void createprofile(String firstname,String lastname, String dob,String phonenumber, String email) {
 		filename = firstname + lastname + "profiledata.txt";
 		firstname = firstname.trim();
 		lastname = lastname.trim();
 		try {
 			FileWriter writer = new FileWriter(filename, true);
 			writer.write("Perseonal Data\n");
-			writer.write(firstname+"\n"+lastname+"\n"+dob+"\n");
+			writer.write(firstname+"\n"+lastname+"\n"+dob+"\n"+phonenumber+"\n"+email+"\n");
 			writer.close();
 			System.out.println("Data entered into file.");
 		} catch (IOException e) {
@@ -30,7 +30,7 @@ public class filewrite {
 		}
 	}
 	
-	public void writevitals(String firstname,String lastname, String weight, String height,String bodytemp,String bodypressure) {
+	public static void writevitals(String firstname,String lastname, String weight, String height,String bodytemp,String bodypressure) {
 		filename = firstname + lastname + "profiledata.txt";
 		
 		File file = new File(filename);
@@ -53,7 +53,7 @@ public class filewrite {
 		}
 	}
 	
-	public void writesummary(String firstname,String lastname, String summary) {
+	public static void writesummary(String firstname,String lastname, String summary) {
 		filename = firstname + lastname + "profiledata.txt";
 		
 		File file = new File(filename);
@@ -77,7 +77,29 @@ public class filewrite {
 		}
 	}
 	
-	public void writehistory(String firstname,String lastname, String previoushealthissues,String previousprescribedmeds, String historyofimmunisation) {
+	public static void writecurrenthealth(String firstname,String lastname,String allergies,String healthconserns) {
+		filename = firstname + lastname + "profiledata.txt";
+		File file = new File(filename);
+		if(file.exists()) {
+			System.out.println("File found.");
+		}
+		else {
+			System.out.print("No file found.");
+			return;
+		}
+		try {
+			FileWriter writer = new FileWriter(filename, true);
+			writer.write("Current Health\n");
+			writer.write(allergies+"\n"+healthconserns+"\n");
+			writer.close();
+			System.out.println("current health entered into file.");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void writehistory(String firstname,String lastname, String previoushealthissues,String previousprescribedmeds, String historyofimmunisation) {
 		filename = firstname + lastname + "profiledata.txt";
 		
 		File file = new File(filename);
